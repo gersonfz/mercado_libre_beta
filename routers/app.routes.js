9ce79177915a28eae039562ec4bc688cc297896a
Gerson Fernández <gersonfz17@gmail.com>
2022-10-10 23:39:57 -0300

    Add cart routes

--- routers/app.routes.js
+++ routers/app.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const productsRoutes = require('./products/products.routes');
+const cartRoutes = require('./cart/cart.routes')
 
 const router = express.Router();
 router.use('/products', productsRoutes);
+router.use('/cart', cartRoutes)
 
 module.exports = router;
